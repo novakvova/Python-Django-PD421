d37607738570588b064d2043479ed522f811c864
novakvova <novakvova@gmail.com>
2026-01-07 16:10:52 +0200

    Update form register user

--- my-vite/src/pages/RegisterPage.tsx
+++ my-vite/src/pages/RegisterPage.tsx
@@ -1,4 +1,5 @@
-import {Button, Form, Input} from "antd";
+import {Button, Form, Input, Upload} from "antd";
+import { InboxOutlined } from '@ant-design/icons';
 import type {IRegisterUser} from "../types/account/IRegisterUser.ts";
 
 const RegisterPage = () => {
@@ -8,45 +9,110 @@ const RegisterPage = () => {
         console.log('Success:', values);
     }
 
+    const normFile = (e: any) => {
+        console.log('Upload event:', e);
+        if (Array.isArray(e)) {
+            return e;
+        }
+        return e?.fileList;
+    };
+
     return (
-        <>
-            <div className="container mx-auto px-4 py-8">
+        <div className={"min-h-screen xl:flex"}>
+            <div className="p-4 mx-auto max-w-(--breakpoint-2xl) md:p-6">
                 <h1 className="mb-4 text-center text-4xl font-bold tracking-tight text-heading md:text-5xl lg:text-6xl">
                     Реєстрація користувача
                 </h1>
 
-                <Form onFinish={onFinish}
-                      form={form}
-                      labelCol={{span: 4}}
-                      wrapperCol={{span: 14}}
-                      layout={"horizontal"}
-                >
-                    <Form.Item<IRegisterUser>
-                        label={"Електронна пошта"}
-                        name={"email"}
-                        rules={[{ required: true, message: 'Вкажіть пошту'}]}
+                <div className="overflow-hidden rounded-2xl
+                    border border-gray-200 bg-white px-4 pb-3 pt-4
+                    dark:border-gray-800 dark:bg-white/[0.03] sm:px-6">
+
+                    <div className="max-w-full overflow-x-auto">
+                        <Form onFinish={onFinish}
+                              className={"w-full"}
+                              form={form}
+                              labelCol={{span: 6}}
+                              wrapperCol={{span: 16}}
+                              layout={"horizontal"}
                         >
-                        <Input/>
-                    </Form.Item>
+                            <Form.Item<IRegisterUser>
+                                label={"Електронна пошта"}
+                                name={"email"}
+                                rules={[{ required: true, message: 'Вкажіть пошту'}]}
+                            >
+                                <Input/>
+                            </Form.Item>
 
-                    <Form.Item<IRegisterUser>
-                        label={"Прізвище"}
-                        name={"lastName"}
-                        rules={[{ required: true, message: 'Вкажіть прізвище'}]}
-                    >
-                        <Input/>
-                    </Form.Item>
+                            <Form.Item<IRegisterUser>
+                                label={"Прізвище"}
+                                name={"lastName"}
+                                rules={[{ required: true, message: 'Вкажіть прізвище'}]}
+                            >
+                                <Input/>
+                            </Form.Item>
 
-                    <Form.Item label={null}>
-                        <Button type="primary" htmlType="submit">
-                            Реєстрація
-                        </Button>
-                    </Form.Item>
+                            <Form.Item<IRegisterUser>
+                                label={"Ім'я"}
+                                name={"firstName"}
+                                rules={[{ required: true, message: "Вкажіть ім'я"}]}
+                            >
+                                <Input/>
+                            </Form.Item>
 
-                </Form>
+                            <Form.Item<IRegisterUser>
+                                label={"Телефон"}
+                                name={"phoneNumber"}
+                                rules={[{ required: true, message: "Вкажіть телефон"}]}
+                            >
+                                <Input/>
+                            </Form.Item>
+
+                            <Form.Item<IRegisterUser>
+                                label={"Пароль"}
+                                name={"password"}
+                                rules={[{ required: true, message: "Вкажіть пароль"}]}
+                            >
+                                <Input.Password/>
+                            </Form.Item>
+
+                            <Form.Item<IRegisterUser>
+                                label={"Підтвердження паролю"}
+                                name={"confirmPassword"}
+                                rules={[
+                                    { required: true, message: "Вкажіть підтвердження пароль"}
+                                ]}
+                            >
+                                <Input.Password/>
+                            </Form.Item>
+
+                            <Form.Item label="Dragger">
+                                <Form.Item name="dragger" valuePropName="fileList"
+                                           getValueFromEvent={normFile}
+                                           noStyle>
+                                    <Upload.Dragger name="files" action="/upload.do">
+                                        <p className="ant-upload-drag-icon">
+                                            <InboxOutlined />
+                                        </p>
+                                        <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                                        <p className="ant-upload-hint">Support for a single or bulk upload.</p>
+                                    </Upload.Dragger>
+                                </Form.Item>
+                            </Form.Item>
+
+                            <Form.Item label={null}>
+                                <Button type="primary" htmlType="submit">
+                                    Реєстрація
+                                </Button>
+                            </Form.Item>
+
+                        </Form>
+                    </div>
+                </div>
+
 
             </div>
-        </>
+        </div>
     );
 }
 
